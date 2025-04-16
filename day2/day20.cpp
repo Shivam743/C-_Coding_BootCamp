4aca6e8a7e057653229cb09f88b5008289fc0094
Shivam Yadav <shivkumar5574240@gmail.com>
2025-04-16 00:11:34 -0700

    end of day2 of coding bootcame

---
+++ day2/day20.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main() {
+   
+
+    // float x = (float)5/2;
+    // std::cout << x << std::endl;
+
+// # simple interest 
+
+    // int p = 1000, r=8,t=5;
+    // int interest = (p*r*t)/100;
+    // std::cout << interest << std::endl;
+
+    char jun = 'a';
+    std::cout << jun << std::endl;
+    int x = jun;
+    std::cout << x << std::endl;
+
+
+
+
+    return 0;
+}
